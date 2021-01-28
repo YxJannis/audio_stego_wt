7b120fef80c91edcb5f51ab11cf28ee31a9b2137
Jannis Leuther <Jannis.Leuther@uni-weimar.de>
2021-01-28 16:34:58 +0100

    more progress in big_level_steganalysis

--- bit_level_steganalysis.py
+++ bit_level_steganalysis.py
@@ -1,26 +1,23 @@
 from audio_file import AudioFile
+import soundfile
 
-audio = AudioFile('testdata/44 Pianisten 01-Promenade.wav')
-audio_data = audio.signal_data
-
-size = len(audio_data)
+file_path = 'testdata/44 Pianisten 01-Promenade.wav'
+signal_data, sampling_rate = soundfile.read(file_path, dtype='int16')
 
 
-count = 0
 suspicious_elements = []
-threshold = 0.18
+threshold = 1000
 
 
-for i in range(2, len(audio_data)-1):
+for i in range(2, len(signal_data)-1):
     # if difference between data[i-1], data [i] und data[i+1], data[i] zu groß erstmal elemente at i, i+1, i-1 ausgeben
-    # TODO: threshold ausprobieren
-    # TODO: wie messen? nur difference zwischen i-1 und i / i+1 und i oder schauen ob die diff between
-    x_0 = audio_data[i-1]
-    x_1 = audio_data[i]
-    x_2 = audio_data[i+1]
-    if (abs(x_1) - (abs(x_0) - abs(x_2))) > threshold:
-        print(f'{x_0}, {x_1}, {x_2}')
-        suspicious_elements.append(i)
+    x_0 = signal_data[i-1]
+    x_1 = signal_data[i]
+    x_2 = signal_data[i+1]
+    if (x_1 > x_0 and x_1 > x_2) or (x_1 < x_0 and x_1 < x_2):
+        if abs(x_1)-abs(x_0) > threshold or abs(x_2)-abs(x_0) > threshold:
+            print(f'{x_0}, {x_1}, {x_2}')
+            suspicious_elements.append(i)
 
 
-print(f'{suspicious_elements}')
+# print(f'{suspicious_elements}')

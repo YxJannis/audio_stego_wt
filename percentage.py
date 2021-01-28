266f4770e5a8fed909244b2205ba44a8d10511c8
Sabbelz <sebastian.reichmann@uni-weimar.de>
2021-01-28 14:36:03 +0100

    percentage is now correctly calculated. Recreate an array for plots

--- percentage.py
+++ percentage.py
@@ -15,34 +15,37 @@ def percentage_audio(audio_org, audio_mod):
     mod_x = []
 
     # get access to the y coordinates
-    for i in range(1726662):
+    for i in range(1000):
         for j in range(1):
-            org_y.append(org[i][0])
+            org_y.append(org[i][1])
 
-    for i in range(1726662):
+    for i in range(1000):
         for j in range(1):
-            mod_x.append(mod[i][0])
+            mod_x.append(mod[i][1])
 
     # calculating the percentage
-    for i in range(1726662):
+    for i in range(1000):
         if org_y[i] == 0:
-            percentage.insert(0, [i])
-        percentage.append((mod_x[i] / org_y[i]) * 100)
-    print(percentage)
+            org_y[i] = 0.00000000000000001
+        value = (mod_x[i]/org_y[i]) * 100
+        percentage.append(value)
     return percentage
 
 
-"""def recreate_signal(percentage):
+def recreate_array(percentage):
     rec_sig = []
     data_x = data[1]
+    data_pair = []
     
-    for i in range(1726662):
+    for i in range(1000):
         for j in range(1):
-            data_x.append([0][i])
+            data_pair.append(data_x[i][0])
 
-    for x, y in zip(1726662):
-        
-    return rec_sig"""
+    for x, y in zip(data_pair, percentage):
+        rec_sig.extend([x, y])
+    print('percentage array: \n')
+    print(rec_sig)
+    return rec_sig
 
 
-percentage_audio(data, data)
+recreate_array(percentage_audio(data, data))

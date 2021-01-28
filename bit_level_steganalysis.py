535b6d9c18f4f456b30fba7ffa5ad7312c0b676d
Jannis Leuther <Jannis.Leuther@uni-weimar.de>
2021-01-28 15:45:06 +0100

    add testing data for bit level steganalysis

--- bit_level_steganalysis.py
+++ bit_level_steganalysis.py
@@ -1,17 +1,26 @@
 from audio_file import AudioFile
 
-audio = AudioFile('')
-audio_data = audio.signal_data.T[0]
+audio = AudioFile('testdata/44 Pianisten 01-Promenade.wav')
+audio_data = audio.signal_data
 
-print(f'{audio_data}\n\n{audio_data[0]}\n\n{audio_data.T}\n\n{audio_data.T[0]}')
+size = len(audio_data)
+
 
 count = 0
+suspicious_elements = []
+threshold = 0.18
 
 
-for i in range(2, len(audio_data)):
+for i in range(2, len(audio_data)-1):
     # if difference between data[i-1], data [i] und data[i+1], data[i] zu groß erstmal elemente at i, i+1, i-1 ausgeben
     # TODO: threshold ausprobieren
-    # TODO: wie messen? nur difference zwischen i-1 und i / i+1 und i oder schauen ob die diff between 
-    prev_c = audio_data[i-1]
-    curr_c = audio_data[i]
-    next_c = audio_data[i+1]
+    # TODO: wie messen? nur difference zwischen i-1 und i / i+1 und i oder schauen ob die diff between
+    x_0 = audio_data[i-1]
+    x_1 = audio_data[i]
+    x_2 = audio_data[i+1]
+    if (abs(x_1) - (abs(x_0) - abs(x_2))) > threshold:
+        print(f'{x_0}, {x_1}, {x_2}')
+        suspicious_elements.append(i)
+
+
+print(f'{suspicious_elements}')
